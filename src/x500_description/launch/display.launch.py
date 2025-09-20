62372a064b4748a5c068a5dd6010dd3e174b48f7
Andres E ibarra <ibarrapaz0405@gmail.com>
2025-09-19 23:02:27 -0600

    description x500

---
+++ src/x500_description/launch/display.launch.py
@@ -0,0 +1,34 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    pkg_path = get_package_share_directory('x500_description')
+    urdf_file = os.path.join(pkg_path, 'urdf', 'x500.urdf')
+
+    with open(urdf_file, 'r') as infp:
+        robot_desc = infp.read()
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            parameters=[{'robot_description': robot_desc}],
+            output='screen'
+        ),
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            output='screen'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=['-d', os.path.join(pkg_path, 'rviz', 'display.rviz')]
+        )
+    ])
